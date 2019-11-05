2edc4c28110bee28b8d6108058cb6bdedaf0001c
Eric Chen <ericc661@gmail.com>
2019-11-04 23:45:04 -0500

    bedtime

--- count_inversions.py
+++ count_inversions.py
@@ -9,10 +9,18 @@ class InversionCounter:
             return (0, A)
 
         # divide array into two pieces
-        cut = (len(A) - 1) // 2 # the last index of the left half
-        L = [5]
+        cut = len(A) // 2 # the first index of the right half
+        L = A[:cut]
+        R = A[cut:]
+        (count_L, list_L) = self.count_inversions(L)
+        (count_R, list_R) = self.count_inversions(R)
 
         return (10, [1, 3, 5]) # return garbage for now
+
+    # given two sorted lists, merges them and counts inversions
+    #   across the boundary
+    #   TODO: define inversions across boundary more clearly
+    def merge_and_count(self, L, R):
 
 def main():
     ic = InversionCounter()
